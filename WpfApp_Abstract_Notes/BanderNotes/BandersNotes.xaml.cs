c0f0f5024ec12ce5fa58166672025ce21ee03d41
Bander Saleh <bandersaleh93@gmail.com>
2023-01-31 21:25:01 -0800

    Notes on Project Showcasing Abstract are Saved in the BandersNotes.xaml.cs subfile. Still missing answers to assignment questions and App Functionality with an Abstract class

---
+++ WpfApp_Abstract_Notes/BanderNotes/BandersNotes.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_Abstract_Notes.BanderNotes
+{
+    /// <summary>
+    /// Interaction logic for BandersNotes.xaml
+    /// </summary>
+    public partial class BandersNotes : Page
+    {
+        public BandersNotes()
+        {
+            InitializeComponent();
+
+            //MY NOTES for the Abstract chapter
+
+            // While using the Visual Studio app to develop a C# Wpf Project,
+            //if you accidentally lose one of your windows, click the Window-
+            //Column at the top of the App and then click "Reset Window Layout"
+
+            // A static constructor only runs ONCE and once and item from the 
+
+            // Our New Wpf App will first run the code from MainWindow.xaml.cs.
+
+            // Our first base parent class of code for our project:
+
+            // MainWindow.xaml.cs (File)
+
+            /*
+            InitializeComponent();
+            
+            // Observable Collection
+            // To attach an ObservableCollection to a Selection Box, Attach it to the ItemsSource
+            
+            lbDisplay.ItemsSource = products;
+
+
+            */
+
+
+            // Then we create the following new class objects to store
+            //more detailed child data that refers back to our original
+            //base parent class object's details
+
+
+            // Product.cs (file)
+            /*
+             // First, I wrote all of my instance details out for Product
+             // that I convert to a constructor by highlighting the instance details
+             // and right clicking on the highlighted data and then choose constructor.
+             
+            // Next, I gave our app some default data to display on our listbox when the App runs
+            void Preload()
+            {
+            products.Add(new ColdDrink([insert details for all 3 parameters of child class added to our listbox]);
+            products.Add(new BakedGood([insert details for all 3 parameters of child class added to our listbox]);
+            products.Add(new Merchandise([insert details for all 3 parameters of child class added to our listbox]);
+            products.Add(new HotDrink([insert details for all 3 parameters of child class added to our listbox]);
+            }
+
+
+
+            */
+
+
+            // Drink.cs (File)
+            /*
+             abstract class Drink : Product
+
+
+            */
+
+            // BakedGood.cs (File)
+            /*
+             
+            */
+
+            // Merchandise.cs (File)
+            /*
+             
+            */
+
+            // ColdDrink.cs (File)
+            /*
+             class ColdDrink : Drink
+            // This child class ColdDrink has a base parent Drink class, and that parent class Drink class is also a child to our original base parent Product class
+            {
+            public ColdDrink(//dataType + objectName for each detail that our ColdDrink instance will contain.)
+            // This code containing the details ^ of our ColdDrink instances
+            //is an example of poly morphism.
+
+            }
+            */
+
+            /*
+             
+
+
+             
+            The App Goal Is To Create:
+
+            Abstract
+
+
+
+
+
+            Questions:
+
+            How do you create an abstract class?
+            Answer:
+
+            Can you create a new instance of an abstract class?
+            Answer:
+            
+            Can you give a body to an abstract method?
+            Answer:
+
+            */
+        }
+    }
+}
